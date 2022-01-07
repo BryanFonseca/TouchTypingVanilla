e658ebd1ecb52fd6c6c585b220ecfd5e38cd9b0e
Bryan Fonseca <isaac1805@hotmail.com>
2022-01-06 23:53:08 -0500

    Refactoring area detection

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-/* const palabras =
-  "Esto es un texto de ejemplo porque es un ejemplo y está siendo un texto a la vez la premisa es sencilla pero no bilateral siguen siendo dos cosas completamente diferentes y estoy intentando que esto sea lo más largo posible estoy seguro de que hola mamá como estás yo igual te voy a sacar de esta basura de país"; */
-const palabras = "Hoy no se considera";
+const palabras =
+  "Esto es un texto de ejemplo porque es un ejemplo y está siendo un texto a la vez la premisa es sencilla pero no bilateral siguen siendo dos cosas completamente diferentes y estoy intentando que esto sea lo más largo posible estoy seguro de que hola mamá como estás yo igual te voy a sacar de esta basura de país";
+/* const palabras = "Hoy no se considera"; */
 class App {
   #touchTyper = new TouchTyper(textContainer, inputElement, palabras);
   constructor() {}
@@ -42,10 +42,6 @@ class TouchTyper {
       .getElement()
       .addEventListener("input", this._startTimer.bind(this), { once: true });
     this.textToType.paintCurrentWord();
-
-    /*     this.textInput.getElement().addEventListener('input', function(){
-      document.styleSheets[0].insertRule('::-webkit-scrollbar-thumb{background: transparent !important}')
-    }, {once: true}); */
   }
   _startTimer() {
     this.#timer.start();
@@ -97,20 +93,20 @@ class TouchTyper {
 }
 
 class TextArea {
-  #currentScroll = 0;
   #textArray;
-  #currentWordIndex = 0;
-  #breakArea;
   #wordsElementArray;
+  #currentWordIndex = 0;
+  #currentScroll = 0;
   constructor(htmlElement, text) {
     this.htmlElement = htmlElement;
     this.#textArray = text.split(" ");
     this._generateText();
-    this.#breakArea = this._generateArea();
     this.#wordsElementArray = [...this.htmlElement.querySelectorAll(".word")];
   }
   paintCurrentWord() {
-    this.#wordsElementArray.forEach((wordEl) => wordEl.classList.remove("word--current"));
+    this.#wordsElementArray.forEach((wordEl) =>
+      wordEl.classList.remove("word--current")
+    );
 
     if (this.#currentWordIndex > this.#textArray.length) return;
 
@@ -157,30 +153,16 @@ class TextArea {
     return this.#currentWordIndex - 1;
   }
   _isInsideArea(element) {
-    //top is distance from top of the viewport to the element
     const { top: elementTop } = element.getBoundingClientRect();
-    const { top: breakAreaTop } = this.#breakArea.getBoundingClientRect();
+    const firstWord = document.querySelector(".word");
     const areaHeight = Number.parseFloat(
-      window.getComputedStyle(this.#breakArea).height
+      window.getComputedStyle(firstWord).height
     );
-    const bottomArea = breakAreaTop + areaHeight;
-
-    /*     if (elementTop > bottomArea) {
-      console.log("Fuera del áre'");
-    } else {
-      console.log("Dentro del área");
-    } */
-
+    const { top: areaTop } = this.htmlElement.getBoundingClientRect();
+    const bottomArea = areaTop + areaHeight;
     return elementTop > bottomArea;
   }
   _generateText() {
-    /*     this.#textArray.forEach((word) => {
-      const markedUpWords = [...word]
-        .map((char) => `<span class="char">${char}</span>`)
-        .join("");
-      const html = `<span class="word">${markedUpWords}</span>`;
-      this.htmlElement.insertAdjacentHTML("beforeend", html);
-    }); */
     const markedUpWords = this.#textArray
       .map((word) => {
         const markedUpChars = [...word]
@@ -192,22 +174,12 @@ class TextArea {
       .join("");
     this.htmlElement.insertAdjacentHTML("beforeend", markedUpWords);
   }
-  _generateArea() {
-    //
-    const firstWord = document.querySelector(".word");
-    const breakArea = document.createElement("div");
-    const component = document.querySelector(".touch-typer");
-    breakArea.classList.add("break-area");
-    breakArea.style.height = window.getComputedStyle(firstWord).height;
-    breakArea.style.width = "100%";
-    component.insertBefore(breakArea, textContainer);
-    return breakArea;
-  }
   _scrollElementHeight(element) {
     const { top: topWordOutOfArea } = element.getBoundingClientRect();
     const { top: topContainer } = this.htmlElement.getBoundingClientRect();
 
-    this.#currentScroll += topWordOutOfArea - topContainer - 5;
+    //6 píxeles menos de scroll
+    this.#currentScroll += topWordOutOfArea - topContainer - 6;
 
     this.htmlElement.scroll({
       top: this.#currentScroll,
